f383c3c650a0dcdd929b0d917a01f08333441e38
Bria222 <briatechologies@gmail.com>
2023-11-02 09:30:28 +0300

    login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
 const CryptoJS = require('crypto-js')
-const user = require('../models/user')
+const User = require('../models/user') // Assuming your user model is exported as "User"
 
 const registerUser = async (req, res, next) => {
-  const newUser = new user({
+  const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: CryptoJS.AES.encrypt(
@@ -19,8 +19,30 @@ const registerUser = async (req, res, next) => {
   }
 }
 
-// login user
 const loginUser = async (req, res, next) => {
-  res.status(200).json({ status: 'succes' })
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      return res.status(401).json('Wrong login credentials') // Handle the case where the user is not found
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    )
+    const decryptedPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+    if (decryptedPassword !== password) {
+      return res.status(401).json('Wrong password credentials')
+    }
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+    console.log(error)
+  }
 }
+
 module.exports = { registerUser, loginUser }
